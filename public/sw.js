8c4598b232964ef595a79865f400388821d1aa7f
01ajay <guptakumarg10@gmail.com>
2023-01-16 16:17:14 +0530

    notification

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,28 @@
 console.log("service worker is running in back ground");
 
-self.addEventListener('push',()=>{
-    self.registarion.sendNotification('Test Message',{});
+self.addEventListener('push',function(e){
+    var options = {
+        body:'This notification generated from push',
+        icon:'',
+        vibrate:[100,50,100],
+        data:{
+            dateOfArrival:Date.now(),
+            primaryKey:2,
+        },
+        actions:[
+            {
+                action:'explore',
+                title:'Explore new ',
+                icon:'',
+            },
+            {
+                action:'close',
+                title:'close ',
+                icon:'',
+            },
+        ]
+    };
+    e.waitUntil(
+    self.registarion.showNotification('Hello Ajay',{})
+    )
 })
