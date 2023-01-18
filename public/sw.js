0a0988a095dd68d565aa43632acd59884635ebee
01ajay <guptakumarg10@gmail.com>
2023-01-18 13:49:47 +0530

    changes sw

--- public/sw.js
+++ public/sw.js
@@ -2,48 +2,43 @@ console.log("service worker is running in back ground");
 
 //let payload = {title:"Indian Bank",body:"Lowest Interest Ever!!",url:""};
 
-
-self.addEventListener("install",()=>{
-    self.skipWaiting();
-})
-
-self.addEventListener('push', (e)=>{
-
-console.log("e.data=>",e);
-   let payload=e.data.json();
-    var options = {
-        body:`${payload?.["body"]}.`,
-        icon:'ib.png',
-        vibrate:[100,50,100],
-        data:{
-            url:payload?.["url"],
-            dateOfArrival:Date.now(),
-            primaryKey:2,
-        },
-        actions:[
-            {
-                action:'go',
-                title:'Let\'s Go',
-            
-            },
-         
-        ]
-    };
-    e.waitUntil(
-    self.registration.showNotification(payload?.["title"],options)
-    );
-    
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("notificationclick",(e)=>{
+self.addEventListener("push", function (e) {
+  console.log("e.data=>", e.data.text());
+  if (e.data) {
+    let payload = e.data.json();
+    var options = {
+      body: `${payload?.["body"]}.`,
+      icon: "ib.png",
+      vibrate: [100, 50, 100],
+      data: {
+        url: payload?.["url"],
+        dateOfArrival: Date.now(),
+        primaryKey: 2,
+      },
+      actions: [
+        {
+          action: "go",
+          title: "Let's Go",
+        },
+      ],
+    };
+    e.waitUntil(
+      self.registration.showNotification(payload?.["title"], options)
+    );
+  } else {
+    console.log("push event has no data ");
+  }
+});
 
-    
-    let payload = e.notification.data;
-    if(e.action === 'go')
-    {
+self.addEventListener("notificationclick", (e) => {
+  let payload = e.notification.data;
+  if (e.action === "go") {
     clients.openWindow(payload?.["url"]);
-    }
-   
+  }
 
-    e.notification.close();
-})
+  e.notification.close();
+});
